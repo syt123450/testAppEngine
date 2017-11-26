d42c275d972d9883b18657b4207f42ec92d517e6
syt123450 <421537891@qq.com>
2017-11-26 13:37:54 -0800

    add files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,8 @@
-package com.test;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String hello() {
 		return "hello world!";
 	}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
